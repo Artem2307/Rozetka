2ca13399561789b701865003295926ef3130cbfc
Artem2307 <Pravdyk1@gmail.com>
2023-02-20 14:08:01 +0200

    fix

--- src/test/java/ApiTests/RequestTest.java
+++ src/test/java/ApiTests/RequestTest.java
@@ -1,8 +1,5 @@
 package ApiTests;
-import io.qameta.allure.Attachment;
-import io.qameta.allure.Description;
-import io.qameta.allure.Story;
-import org.junit.Test;
+
 
 
 
@@ -10,11 +7,6 @@ import org.junit.Test;
 public class RequestTest extends BaseTestApi {
 
 
-    @Test
-    @Story("getUsersFalse")
-    @Description("getUsersFalse")
-    @Attachment
-    public void getUsersFalse(){
-        userPage.getUsersFalse();
-    }
+
+
 }
