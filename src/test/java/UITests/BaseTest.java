966c753e81ffc00d889b143cf31ed2a46512f276
Artem2307 <Pravdyk1@gmail.com>
2023-02-20 12:23:05 +0200

    pom.xml

--- src/test/java/UITests/BaseTest.java
+++ src/test/java/UITests/BaseTest.java
@@ -7,9 +7,7 @@ import Pages.SearchPage;
 
 import Settings.SelenidSetting;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Step;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.After;
 import org.junit.Before;
 
@@ -28,15 +26,13 @@ public class BaseTest {
     public  void setUp() {
         selenidSetting.setting();
         Selenide.open("");
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true)
-                .savePageSource(true));
     }
 
 
     @After
     @Step("Close URL")
     public void tearDown(){
-        Selenide.closeWebDriver();
+        Selenide.close();
         System.out.println("Finish");
     }
 
