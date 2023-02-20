3911ae3a0e2a32c8d95ac393eebc06e5b9d97fe4
Artem2307 <Pravdyk1@gmail.com>
2023-02-20 13:39:34 +0200

    fix

--- src/test/java/UITests/ProductTest.java
+++ src/test/java/UITests/ProductTest.java
@@ -19,7 +19,7 @@ public class ProductTest extends BaseTest {
     @Attachment
     public void BuyProduct(){
         searchPage.search("Iphone 13")
-                .clickProduct("Мобільний телефон Apple iPhone 13 128GB Starlight");
+                .clickProduct("Apple iPhone 13 128GB Starlight");
         productPage.clickBuyButton(" Купити ")
                 .modalVisibiliti();
     }
