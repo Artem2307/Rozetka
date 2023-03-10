f6356847405e4d9448d3187c863fb1f8fea2d253
Artem2307 <Pravdyk1@gmail.com>
2023-03-10 13:42:30 +0200

    FSD-0000:implement ApiTest

--- jenkins.groovy
+++ jenkins.groovy
@@ -26,23 +26,6 @@ node {
                 generateAllure()
             }
         }
-
-//        try {
-//            stage("Run tests") {
-//                parallel(
-//                        'Api Tests': {
-//                            runTestWithTag("apiTests")
-//                        },
-//                        'Ui Tests': {
-//                            runTestWithTag("uiTests")
-//                        }
-//                )
-//            }
-//        } finally {
-//            stage("Allure") {
-//                generateAllure()
-//            }
-//        }
     }
 }
 
